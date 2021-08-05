2dacb8ae1d91398be786fba305737c389d07c690
mrkuz <markus@bitsandbobs.net>
2021-08-05 15:04:24 +0200

    Move build properties configuration to blueprint

--- plugin/src/main/kotlin/net/bnb1/kradle/plugins/BuildPropertiesPlugin.kt
+++ plugin/src/main/kotlin/net/bnb1/kradle/plugins/BuildPropertiesPlugin.kt
@@ -9,8 +9,5 @@ class BuildPropertiesPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.create("generateBuildProperties", "Generates build.properties", GenerateBuildPropertiesTask::class.java)
-        project.tasks.named("processResources").configure {
-            finalizedBy("generateBuildProperties")
-        }
     }
 }
