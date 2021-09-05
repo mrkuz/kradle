47754f639b1a5989c7db0c4698b7d2b1a7bd8029
mrkuz <markus@bitsandbobs.net>
2021-09-05 14:55:25 +0200

    Add bootstrap tests

--- plugin/src/main/kotlin/net/bnb1/kradle/tasks/BootstrapLibTask.kt
+++ plugin/src/main/kotlin/net/bnb1/kradle/tasks/BootstrapLibTask.kt
@@ -3,9 +3,7 @@ package net.bnb1.kradle.tasks
 open class BootstrapLibTask : AbstractBoostrapTask() {
 
     override fun stageTwo() {
-        if (project.group.toString().isEmpty()) {
-            project.logger.warn("WARNING: Group is not specified")
-        } else {
+        if (project.group.toString().isNotEmpty()) {
             val packagePath = project.group.toString().replace(".", "/")
             listOf("main", "test").forEach {
                 project.rootDir.resolve("src/$it/kotlin/$packagePath").mkdirs()
