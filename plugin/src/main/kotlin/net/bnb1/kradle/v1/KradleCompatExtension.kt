12cce75fd2fed4c5a90cbe873dbbcad56238426d
mrkuz <markus@bitsandbobs.net>
2021-12-23 21:47:47 +0100

    Use catalog in compat extension

--- plugin/src/main/kotlin/net/bnb1/kradle/v1/KradleCompatExtension.kt
+++ plugin/src/main/kotlin/net/bnb1/kradle/v1/KradleCompatExtension.kt
@@ -1,5 +1,6 @@
 package net.bnb1.kradle.v1
 
+import Catalog
 import net.bnb1.kradle.empty
 import net.bnb1.kradle.property
 import org.gradle.api.model.ObjectFactory
@@ -19,10 +20,10 @@ open class KradleCompatExtension @Inject constructor(private val factory: Object
     val mainClass
         get() = _mainClass.getOrElse("")
 
-    val targetJvm = factory.property("17")
+    val targetJvm = factory.property(Catalog.Versions.jvm)
     fun targetJvm(version: String) = targetJvm.set(version)
 
-    val kotlinxCoroutinesVersion = factory.property("1.5.2")
+    val kotlinxCoroutinesVersion = factory.property(Catalog.Versions.kotlinCoroutines)
     fun kotlinxCoroutinesVersion(version: String) = kotlinxCoroutinesVersion.set(version)
 
     val tests = TestsExtension(factory)
@@ -38,15 +39,15 @@ open class KradleCompatExtension @Inject constructor(private val factory: Object
     val jacocoVersion = tests.jacocoVersion
     fun jacocoVersion(version: String) = tests.jacocoVersion.set(version)
 
-    val jmhVersion = factory.property("1.33")
+    val jmhVersion = factory.property(Catalog.Versions.jmh)
     fun jmhVersion(version: String) = jmhVersion.set(version)
 
-    val ktlintVersion = factory.property("0.43.0")
+    val ktlintVersion = factory.property(Catalog.Versions.ktlint)
     fun ktlintVersion(version: String) = ktlintVersion.set(version)
 
     val detektConfigFile = factory.property("detekt-config.yml")
     fun detektConfigFile(name: String) = detektConfigFile.set(name)
-    val detektVersion = factory.property("1.18.1")
+    val detektVersion = factory.property(Catalog.Versions.detekt)
     fun detektVersion(version: String) = detektVersion.set(version)
 
     open class UberJarExtension(factory: ObjectFactory) {
@@ -57,13 +58,13 @@ open class KradleCompatExtension @Inject constructor(private val factory: Object
 
     open class ImageExtension(factory: ObjectFactory) {
 
-        val baseImage = factory.property("bellsoft/liberica-openjdk-alpine:17")
+        val baseImage = factory.property("bellsoft/liberica-openjdk-alpine:${Catalog.Versions.jvm}")
         fun baseImage(name: String) = baseImage.set(name)
 
         val ports = factory.setProperty(Int::class.java)
 
         val jvmKillVersion = factory.empty<String>()
-        fun withJvmKill(version: String = "1.16.0") = jvmKillVersion.set(version)
+        fun withJvmKill(version: String = Catalog.Versions.jvmKill) = jvmKillVersion.set(version)
 
         val withAppSh = factory.property(false)
         fun withAppSh() = withAppSh.set(true)
@@ -74,16 +75,16 @@ open class KradleCompatExtension @Inject constructor(private val factory: Object
 
     open class TestsExtension(factory: ObjectFactory) {
 
-        val junitJupiterVersion = factory.property("5.8.1")
+        val junitJupiterVersion = factory.property(Catalog.Versions.junit)
         fun junitJupiterVersion(version: String) = junitJupiterVersion.set(version)
 
-        val jacocoVersion = factory.property("0.8.7")
+        val jacocoVersion = factory.property(Catalog.Versions.jacoco)
         fun jacocoVersion(version: String) = jacocoVersion.set(version)
 
         val kotestVersion = factory.empty<String>()
-        fun useKotest(version: String = "4.6.3") = kotestVersion.set(version)
+        fun useKotest(version: String = Catalog.Versions.kotest) = kotestVersion.set(version)
 
         val mockkVersion = factory.empty<String>()
-        fun useMockk(version: String = "1.12.1") = mockkVersion.set(version)
+        fun useMockk(version: String = Catalog.Versions.mockk) = mockkVersion.set(version)
     }
 }
