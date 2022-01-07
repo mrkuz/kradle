ed23425d78fb2cde1bafe2f84b6c098728c33707
mrkuz <markus@bitsandbobs.net>
2022-01-07 11:03:47 +0100

    Clean up mocks

--- plugin/src/main/kotlin/net/bnb1/kradle/KradleContext.kt
+++ plugin/src/main/kotlin/net/bnb1/kradle/KradleContext.kt
@@ -6,13 +6,13 @@ import kotlin.reflect.full.isSubclassOf
 open class KradleContext {
 
     private val _map = mutableMapOf<KClass<*>, Any>()
-    val map1: Map<KClass<*>, Any>
+    val map: Map<KClass<*>, Any>
         get() = _map
 
     fun register(instance: Any): Boolean = _map.putIfAbsent(instance::class, instance) == null
 
     @Suppress("UNCHECKED_CAST")
-    inline fun <reified T : Any> get() = map1[T::class] as T
+    inline fun <reified T : Any> get() = map[T::class] as T
 
     @Suppress("UNCHECKED_CAST")
     operator fun <T : Any> get(key: KClass<T>) = _map[key] as T
